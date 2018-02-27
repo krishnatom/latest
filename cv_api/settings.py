e8dc51fbecdc0ac60b4ffee19a747f6ded273d25
Kaarthik Ananth.B <krishnakumar31096@gmailcom>
2018-02-27 11:25:19 +0530

    Deploying the code into heroku

--- cv_api/settings.py
+++ cv_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tx0w^1axnf%_4#tde5u*tj9whdmgb0x-o1!g%sothj^^c&jd1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
