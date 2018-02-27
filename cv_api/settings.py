91b48dea7d4e86612daf8e960181b6550503939b
Kaarthik Ananth.B <krishnakumar31096@gmailcom>
2018-02-27 12:34:09 +0530

    test

--- cv_api/settings.py
+++ cv_api/settings.py
@@ -133,16 +133,16 @@ DATABASES['default'].update(db_from_env)
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+'''
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-
+'''
 
 
 
