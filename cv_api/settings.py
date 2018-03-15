4a2c70895c3f2681cfbf30dda9e61c6e095d3db3
krishnakumar <krishnakumar31096@gmail.com>
2018-03-15 14:15:34 +0530

    Update settings.py

--- cv_api/settings.py
+++ cv_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'tx0w^1axnf%_4#tde5u*tj9whdmgb0x-o1!g%sothj^^c&jd1('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
