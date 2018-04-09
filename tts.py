073857bc6d6a0fd1492d897c130b6a6c1bad246c
krishnatom <krishnakumar31096@gmailcom>
2018-04-10 01:00:42 +0530

    njn
    kglkb

--- tts.py
+++ tts.py
@@ -1,6 +1,15 @@
-import pyttsx3
+import pickle
+import cv2 as cv
+points_pickle = pickle.load( open( "object_and_image_points.pkl", "rb" ) )
+chess_points = points_pickle["chesspoints"]
+image_points = points_pickle["imagepoints"]
+img_size = points_pickle["imagesize"]
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(chess_points, image_points, img_size, None, None)
+camera = pickle.load(open( "camera_matrix.pkl", "rb" ))
+mtx = camera['mtx']
+dist = camera['dist']
+camera_img_size = camera['imagesize']
+print(camera)
 
-engine = pyttsx3.init()
-engine.say('Greetings!')
-engine.say('How are you today?')
-engine.runAndWait()
+
+print(dist)
